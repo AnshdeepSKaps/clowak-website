a9ec573a82e83ee82cc835b17602fc6c2c3dd517
anshdeep <kapulaanshdeep@gmail.com>
2022-11-23 17:00:56 +0530

    more responseiveness and burger menu

--- src/components/aboutUs/aboutUs.js
+++ src/components/aboutUs/aboutUs.js
@@ -13,7 +13,7 @@ export default function Aboutus() {
     return (
         <div id="about-us-page" className="d-flex flex-column justify-content-between align-items-center">
             <h1 className="header text-center" style={{ fontWeight: "700", color: vars.darkGreen }}>ABOUT US</h1>
-            <p className="home-text mt-3 text-center" style={{ textAlign: "justify", fontWeight: "600", fontSize: "1.3rem" }}>We, at Clowak Innovations, strive to create ingenious products that focus on making the
+            <p className="home-text mt-3 text-center" style={{ textAlign: "justify" }}>We, at Clowak Innovations, strive to create ingenious products that focus on making the
                 lives of our customers easier, affordable, and effortlessly accessible.
                 Our products are made in India, designed by our Indian engineers, and manufactured at our
                 industry in Vellore, Tamil Nadu.
